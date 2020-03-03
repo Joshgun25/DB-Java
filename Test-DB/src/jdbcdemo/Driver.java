a0a90ef502f3d770ae0ede386a69083faf877803
Joshgun25 <joshgun.rzabayli@gmail.com>
2020-03-04 01:28:48 +0300

    Added new queries

--- Test-DB/src/jdbcdemo/Driver.java
+++ Test-DB/src/jdbcdemo/Driver.java
@@ -6,14 +6,29 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
+			
+			// Make Connection
 			Connection myCon = DriverManager.getConnection("jdbc:mysql://localhost:3306/sys","joshgun","542599");
 			
+			// Create Statement
 			Statement myStmt = myCon.createStatement();
 			
-			ResultSet myRs = myStmt.executeQuery("select * from country");
+			// Insert Query
+			String sql = "insert into country" + "(name, capital, population, area)" + "values('Norway', 'Oslo', '554554','223232323')";
+			myStmt.executeUpdate(sql);
 			
+			// Update Query
+			sql = "update country set capital = 'Ankara' where id=2";
+			myStmt.executeUpdate(sql);
+			
+			// Delete Query
+			sql = "delete from country where name='Russia'";
+			myStmt.executeUpdate(sql);
+			
+			// Select Query
+			ResultSet myRs = myStmt.executeQuery("select * from country");
 			while(myRs.next()) {
-				System.out.println(myRs.getString("NAME") + "," + myRs.getString("CAPITAL"));
+				System.out.println(myRs.getString("ID") + "," + myRs.getString("NAME") + "," + myRs.getString("CAPITAL"));
 			}
 		}
 		
